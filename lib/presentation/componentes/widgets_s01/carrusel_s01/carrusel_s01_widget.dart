ab0c48ed42edfd26f616cbb6da41dc986a8c4ab1
JonathanG1702 <jonathang1702yt@gmail.com>
2024-01-21 19:58:12 +0100

    Corregimos un error en la ruta de una imagen

--- lib/presentation/componentes/widgets_s01/carrusel_s01/carrusel_s01_widget.dart
+++ lib/presentation/componentes/widgets_s01/carrusel_s01/carrusel_s01_widget.dart
@@ -174,7 +174,7 @@ class _CarruselS01WidgetState extends State<CarruselS01Widget> {
             ClipRRect(
               borderRadius: BorderRadius.circular(8.0),
               child: Image.asset(
-                'assets/images/carruseles/carrusel_1/carrusel_Imagen10.png',
+                'assets/images/carruseles/carrusel_s01/carrusel_Imagen10.png',
                 fit: BoxFit.cover,
                 alignment: Alignment(-0.35, 0.0),
               ),
